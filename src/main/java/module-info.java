e3c4b8739c24025eeed8d7968d1ea5ecf89dad6c
Anders Orback <anders.orback@gmail.com>
2022-10-02 15:00:56 +0200

    Work in progress

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,5 @@
 module JavaFXHelper {
   exports se.centernode.javafx.helper.components;
-  exports se.centernode.javafx.helper.sample;
   requires javafx.base;
   requires javafx.graphics;
   requires javafx.controls;
